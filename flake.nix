9f4243a64c5713d2edf4562fb6d6776c67e1fa1d
Naiara Gomez Castro <git@naiara.one>
2025-12-18 21:48:03 -0300

    create a bun dev shell

--- flake.nix
+++ flake.nix
@@ -1,4 +1,19 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
-  outputs = { }: { };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      inherit (nixpkgs) lib;
+
+      inherit (lib.attrsets) genAttrs;
+      inherit (lib.systems) flakeExposed;
+
+      eachSystem = f: genAttrs flakeExposed (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell { packages = [ pkgs.bun ]; };
+      });
+    };
 }
