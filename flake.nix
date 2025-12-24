b5d945567aa5c3d984422e5ce9f4ccc71c9f3d44
Naiara Gomez Castro <git@naiara.one>
2025-12-23 21:17:26 -0300

    go back to astro

--- flake.nix
+++ flake.nix
@@ -1,42 +1,17 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
-
-    rust-overlay = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:oxalica/rust-overlay";
-    };
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
 
   outputs =
-    { nixpkgs, rust-overlay, ... }:
+    { nixpkgs, ... }:
     let
       inherit (nixpkgs) lib;
-      eachSystem = lib.genAttrs lib.systems.flakeExposed;
+      eachSystem = f: lib.genAttrs lib.systems.flakeExposed (system: f nixpkgs.legacyPackages.${system});
     in
     {
-      devShells = eachSystem (system: {
-        default =
-          let
-            overlays = [ (import rust-overlay) ];
-            pkgs = import nixpkgs { inherit system overlays; };
-
-            rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-              targets = [ "wasm32-unknown-unknown" ];
-            };
-          in
-          pkgs.mkShell {
-            RUST_BACKTRACE = "1";
-            RUSTFLAGS = "--cfg erase_components";
-
-            packages = with pkgs; [
-              leptosfmt
-              rust-analyzer
-              rustfmt
-              rustToolchain
-              trunk
-            ];
-          };
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          packages = [ pkgs.bun ];
+        };
       });
     };
 }
